ce4ef09e0ae291701beced31c1314f241d8d888f
Matt Thompson <matt34thompson@gmail.com>
2018-12-30 21:02:53 +0000

    Adds more redacted examples

--- ruby/ruby_codecademy/redacted.rb
+++ ruby/ruby_codecademy/redacted.rb
@@ -10,14 +10,12 @@ words = text.split(" ") # <- takes a string and returns an array, divided wherev
 redacted_text = []
 
 words.each do |word|
-  redact_array.each do |reacted_word|
-    if word.downcase != redacted_word.downcase
-      # print "#{word} "
-      redacted_text.push("#{word} ")
-    else
-      # print "REDACTED "
-      redacted_text.push("REDACTED ")
-    end
+  if word.downcase != redact.downcase
+    # print "#{word} "
+    redacted_text.push("#{word} ")
+  else
+    # print "REDACTED "
+    redacted_text.push("REDACTED ")
   end
 end
 
@@ -29,7 +27,7 @@ puts redacted_string
 Mess around with your redactor to see what sorts of results you get. Think about the following:
 
 1. What could you do to make sure your redactor redacts a word regardless of whether it's upper case or lower case? DONE
-2. How could you make your program take multiple, separate words to REDACT?
+2. How could you make your program take multiple, separate words to REDACT? IN PRG!
 3. How might you make a new redacted string and save it as a variable, rather than just printing it to the console? DONE
 
 =end
