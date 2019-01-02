23f075e00082e3f05f6592deaeb201ba6f20618f
Matt Thompson <mattthompson@johnlewis.co.uk>
2019-01-02 17:07:04 +0000

    Adds histogram example

---
+++ ruby/ruby_codecademy/histogram.rb
@@ -0,0 +1,13 @@
+puts "Enter a phrase you'd like to analyze: "
+text = gets.chomp
+
+words = text.split(" ")
+frequencies = Hash.new(0) # <- default value (0) - when trying to access a non-existent key, it will have '0' you will get the default value returned
+
+words.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by { |word_key, count| count }
+frequencies.reverse!
+
+frequencies.each do |word_key, count|
+  puts "#{word_key} #{count.to_s}"
+end
