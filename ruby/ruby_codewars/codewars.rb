3425e624df4a0bd0659aadd722f0d16dfaf4ef0c
Matt Thompson <mattthompson@johnlewis.co.uk>
2019-01-04 17:11:21 +0000

    WIP codewars kata

--- ruby/ruby_codewars/codewars.rb
+++ ruby/ruby_codewars/codewars.rb
@@ -23,3 +23,57 @@ end
 puts get_middle("hello") # => "l"
 puts get_middle("test") # => "es"
 puts get_middle("middle")
+
+
+# 3. Equal sides of an array
+
+# You are going to be given an array of integers.
+# Find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
+# If there is no index that would make this happen, return -1.
+
+def find_even_index(arr)
+  arr.each_index do |index|
+    if index == 0
+      a = 0
+      b =  
+    else
+      # 
+    end
+
+    # add all items in the array at positions lower than index -> set equal to 'a'
+    # add all items in the array at positions higher than items -> set equal to 'b'
+    if a == b
+      return item array position
+    else
+      return -1 # <- don't want to do this for all a != b iterations though (just once if none equal)
+    end
+  end
+end
+
+
+arr = {1,2,3,4,3,2,1}
+
+puts find_even_index(arr)
+
+
+=begin
+
+think i need to create 2 new arrays... one with the values below index, another with the values above index
+-> array.slice(index)
+
+i = 0
+arr.each_index |index| (OR for index in arr...)
+  create these arrays
+  sum all values in each array (a.sum AND b.sum)
+  if sum_arr_a == sum_arr_b
+    return index
+  else
+    index += 1
+  end
+
+  if no index returns true for sum_arr_a == sum_arr_b
+    return -1
+  end
+end
+
+=end
